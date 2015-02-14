8c7fb6edb2a7a34316f1c129b0763fb18946babf
Ryan Carey <RyanCarey@users.noreply.github.com>
2015-02-14 23:09:42 +0000

    Update Loss.jl

--- src/Loss.jl
+++ src/Loss.jl
@@ -1,3 +1,40 @@
+export mean_absolute_deviation
+export median_absolute_deviation
+export maximum_absolute_deviation
+export minimum_absolute_deviation
+export root_mean_absolute_deviation
+export sum_absolute_deviation
+export mean_squared_error
+export median_squared_error
+export maximum_squared_error
+export minimum_squared_error
+export root_mean_squared_error
+export sum_squared_error
+export mean_squared_log_error
+export median_squared_log_error
+export maximum_squared_log_error
+export minimum_squared_log_error
+export root_mean_squared_log_error
+export sum_squared_log_error
+export mean_hinge_loss
+export median_hinge_loss
+export maximum_hinge_loss
+export minimum_hinge_loss
+export root_mean_hinge_loss
+export sum_hinge_loss
+export mean_zero_one_loss
+export median_zero_one_loss
+export maximum_zero_one_loss
+export minimum_zero_one_loss
+export root_mean_zero_one_loss
+export sum_zero_one_loss
+export mean_log_loss
+export median_log_loss
+export maximum_log_loss
+export minimum_log_loss
+export root_mean_log_loss
+export sum_log_loss
+
 module Loss
     function absolute_deviation(true_value::Real, predicted_value::Real)
         return abs(true_value - predicted_value)
@@ -31,7 +68,6 @@ module Loss
                 :hinge_loss, :zero_one_loss, :log_loss)
     aggregation_fs = (:mean, :median, :maximum, :minimum, :root_mean, :sum)
 
-    # TODO: Generate export statements?
     for error_f in error_fs
         for aggregation_f in aggregation_fs
             f_name = symbol(string(aggregation_f, "_", error_f))
